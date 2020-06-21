2524229bdf5d371734e832bbfd3c2f4b2b3af299
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-06-21 13:56:23 +0700

    lala

--- api/helper.php
+++ api/helper.php
@@ -16,11 +16,6 @@ if ($host !== "localhost") {
 if (getenv('APP_ENV') === 'development') {
     $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
     $dotenv->load();
-} else {
-    if ($_SERVER["HTTPS"] != "on") {
-        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-        exit();
-    }
 }
 
 function env($env, $default)
