aed88f210c028d4668c98f088e9f1da25f9c50c6
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-06-22 23:31:50 +0700

    logout unset cookie

--- api/actions/logout.php
+++ api/actions/logout.php
@@ -5,4 +5,9 @@ include_once("../helper.php");
 include_once("../database.php");
 session_start();
 unset($_SESSION['user_id']);
+if (isset($_COOKIE['remember_me'])) {
+    $remember_token = $_COOKIE['remember_me'];
+    mysqli_query($conn, "UPDATE `users` SET `remember_token` = '' WHERE `remember_token` = '$remember_token';");
+    setcookie("remember_me", "", time() - 30);
+}
 header("location:" . BASE_URL . "index.php");
