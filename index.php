ceb8142e29594122426656b05d4c92f2d59cea28
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-06-23 22:17:22 +0700

    https redirect

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ require_once("./api/database.php");
 require_once("./api/controller/Auth.php");
 require_once("./api/controller/route.php");
 
-if (!isset($_SERVER['HTTPS'])) {
-    redirectTohttps();
-}
+// if (!isset($_SERVER['HTTPS'])) {
+//     redirectTohttps();
+// }
 
 use Api\Actions\Route;
 
