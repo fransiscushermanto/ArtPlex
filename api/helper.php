d8a5e18273a8b1eb2c3fd7d3934408b2fa2499da
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-06-23 21:56:19 +0700

    https redirect

--- api/helper.php
+++ api/helper.php
@@ -46,6 +46,7 @@ function redirectTohttps()
             $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
             header('HTTP/1.1 301 Moved Permanently');
             header("Location:$redirect");
+            exit;
         }
     }
 }
