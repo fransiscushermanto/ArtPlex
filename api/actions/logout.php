351deab30e1a5ebd0b9607cafdda996d3d54bf08
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-07-01 20:01:53 +0700

    fix bugs register

--- api/actions/logout.php
+++ api/actions/logout.php
@@ -5,9 +5,9 @@ include_once("../helper.php");
 include_once("../database.php");
 session_start();
 unset($_SESSION['user_id']);
-if (isset($_COOKIE['remember_me'])) {
-    $remember_token = $_COOKIE['remember_me'];
+if (isset($_COOKIE['remember_token'])) {
+    $remember_token = $_COOKIE['remember_token'];
     mysqli_query($conn, "UPDATE `users` SET `remember_token` = '' WHERE `remember_token` = '$remember_token';");
-    setcookie("remember_me", "", time() - 30);
+    setcookie("remember_token", "", time() - 30);
 }
 header("location:" . BASE_URL . "index.php");
