9347c6fc3301353513a94b4f93a6732b9da51255
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-07-04 22:39:24 +0700

    test cookie -2

--- api/actions/logout.php
+++ api/actions/logout.php
@@ -7,7 +7,8 @@ session_start();
 unset($_SESSION['user_id']);
 if (isset($_COOKIE['remember_token'])) {
     $remember_token = $_COOKIE['remember_token'];
+    $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
     mysqli_query($conn, "UPDATE `users` SET `remember_token` = '' WHERE `remember_token` = '$remember_token';");
-    setcookie("remember_token", "", time() - 3600);
+    setcookie("remember_token", "", time() - 3600, '/', $domain, false);
 }
 header("location:" . BASE_URL . "index.php");
