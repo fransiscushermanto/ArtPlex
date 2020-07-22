e3e1e0284427a4aaa3bfdce38c66763f14c3ec65
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2020-07-22 19:30:39 +0700

    s3 adjust

--- api/database.php
+++ api/database.php
@@ -6,13 +6,15 @@ $database = env("DB_DATABASE", "none");
 
 $conn = mysqli_connect($host, $username, $password, $database) or die("Failed when try to connecting db " . mysqli_connect_error());
 
-$s3 = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'   => 'ap-southeast-1',
-    'credentials' => array(
-        'key' => env("AWS_ACCESS_KEY_ID", "none"),
-        'secret'  => env("AWS_SECRET_ACCESS_KEY", "none"),
-    )
-]);
+if (getenv('APP_ENV') !== 'development') {
+    $s3 = new Aws\S3\S3Client([
+        'version' => 'latest',
+        'region'   => 'ap-southeast-1',
+        'credentials' => array(
+            'key' => env("AWS_ACCESS_KEY_ID", "none"),
+            'secret'  => env("AWS_SECRET_ACCESS_KEY", "none"),
+        )
+    ]);
 
-$bucket = env('S3_BUCKET_NAME', "none") ?: die('No "S3_BUCKET_NAME" config var in found in env!');
+    $bucket = env('S3_BUCKET_NAME', "none") ?: die('No "S3_BUCKET_NAME" config var in found in env!');
+}
